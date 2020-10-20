6deb7127bdd59bda062e7a713f5930c247faa256
daleclack <63892434+daleclack@users.noreply.github.com>
2020-10-21 07:46:50 +0800

    Add files via upload

---
+++ cpp/xeinit4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+	int a=0,b,c;
+	printf("Input circulate number");
+	scanf("%d",&c);
+	for(int i=0;i<c;i++){
+	a=rand();
+	b=a;
+	a%=2;
+	//getchar();
+	}
+	printf("%d %d\n",a,b);
+	return 0;
+}
