dd98a4acb1c795da2125e853b12f0dd077d8bf6e
daleclack <daleclack1@foxmail.com>
2020-11-29 22:43:38 +0800

    Update gtk18

--- Gtk3/gtk18/main.cpp
+++ Gtk3/gtk18/main.cpp
@@ -42,5 +42,6 @@ int main(int argc,char *argv[]){
     app=gtk_application_new("com.github.daleclack.gtk18",G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app,"activate",G_CALLBACK(activate),NULL);
     status=g_application_run(G_APPLICATION(app),argc,argv);
+    g_object_unref(app);
     return status;
 }
