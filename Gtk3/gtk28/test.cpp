113c30e599c94139925afbd99b129edad8810944
daleclack <daleclack1@foxmail.com>
2020-12-27 10:19:41 +0800

    Update gtk28

--- Gtk3/gtk28/test.cpp
+++ Gtk3/gtk28/test.cpp
@@ -6,6 +6,7 @@
 
 void print(GtkWidget *widget,gpointer data){
     const gchar *filename;
+    //Use the combo box id to check the selection
     int id=gtk_combo_box_get_active(GTK_COMBO_BOX(data));
     switch(id){
         case 0:
@@ -43,6 +44,7 @@ void config2_activate(GtkWidget *widget,gpointer data){
 }
 
 void InputBox(const gchar *content,const char *filename){
+    //Initalize InputBox
     const gchar *str;
     int response;
     GtkWidget *dialog=gtk_dialog_new();
@@ -53,6 +55,7 @@ void InputBox(const gchar *content,const char *filename){
     gtk_window_set_title(_dialog,"Change config");
     gtk_dialog_add_buttons(dialog1,"Cancel",GTK_RESPONSE_CANCEL,"OK",GTK_RESPONSE_OK,NULL);
 
+    //Get content_area and put entry and label to the dialog
     GtkWidget *content_area=gtk_dialog_get_content_area(GTK_DIALOG(_dialog));
     GtkContainer *_content_area=GTK_CONTAINER(content_area);
     GtkWidget *label=gtk_label_new(" ");
@@ -68,6 +71,7 @@ void InputBox(const gchar *content,const char *filename){
     gtk_container_add(_content_area,entry);
     gtk_widget_show_all(content_area);
     response=gtk_dialog_run(dialog1);
+    //Check the response and update the file with the "OK" Response
     if(response==GTK_RESPONSE_OK){
         str=gtk_entry_get_text(GTK_ENTRY(entry));
         freopen(filename,"w",stdout);
@@ -85,6 +89,7 @@ void entry_activate(GtkWidget *widget,gpointer data){
 }
 
 void MsgBox(const gchar *title,const gchar *msg){
+    //Initalize the MsgBox
     GtkWidget *dialog=gtk_dialog_new();
     GtkWindow *win=GTK_WINDOW(dialog);
     gtk_window_set_default_size(win,300,150);
@@ -92,6 +97,7 @@ void MsgBox(const gchar *title,const gchar *msg){
     gtk_window_set_title(win,title);
     gtk_dialog_add_button(GTK_DIALOG(dialog),"OK",GTK_RESPONSE_OK);
 
+    //Get GtkDialog's content_area and put widgets
     GtkWidget *content_area=gtk_dialog_get_content_area(GTK_DIALOG(dialog));
     GtkContainer *_content_area=GTK_CONTAINER(content_area);
     GtkWidget *label=gtk_label_new(" ");
@@ -105,6 +111,7 @@ void MsgBox(const gchar *title,const gchar *msg){
     label=gtk_label_new(msg);
     hbox.pack_start(label,FALSE,FALSE,5);
 
+    //Add a GtkBox to the dialog and run
     gtk_container_add(_content_area,hbox.hbox);
     gtk_widget_show_all(content_area);
     gtk_dialog_run(GTK_DIALOG(dialog));
