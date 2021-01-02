b73e6fbf490e81810893656a8709651e1ed8465b
daleclack <daleclack1@foxmail.com>
2021-01-02 16:47:53 +0800

    Add filename test

---
+++ cpp/filename_test/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+typedef void(*pfun)();
+
+void dale(){
+    printf("custom filename test by daleclack\n");
+}
+
+void write1(){
+    getchar();
+    char str[57];
+    fgets(str,sizeof(str),stdin);
+    freopen("test","w",stdout);
+    printf("%s",str);
+    fclose(stdout);
+}
+
+void test(){
+    char filename[57];
+    FILE *fp;
+    fp=fopen("test","r");
+        if(fp==NULL){printf("File not exist!");return ;}
+        fgets(filename,57,fp);
+    fclose(fp);
+    int length=strlen(filename);
+    filename[length-1]='\0';
+    fp=fopen(filename,"w");
+    fclose(fp);
+}
+
+pfun a[]={dale,write1,test};
+
+int main(int argc,char *argv[])
+{
+    int x;
+    printf("1.write content 2.test the custom filename\n");
+    scanf("%d",&x);
+    a[x]();
+    return 0;
+}
