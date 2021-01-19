6ca3df73c36ee88111a84d4c2a98a8e80d638129
daleclack <daleclack1@foxmail.com>
2021-01-19 13:32:31 +0800

    Update gtk35

--- Gtk3/gtk35/main.cpp
+++ Gtk3/gtk35/main.cpp
@@ -1,9 +1,27 @@
 #include <gtk/gtk.h>
 
+void file_dialog(GtkWidget *widget,gpointer data){
+    GtkFileChooserAction action=GTK_FILE_CHOOSER_ACTION_OPEN;
+    GtkWidget *dialog=gtk_file_chooser_dialog_new("Open File",GTK_WINDOW(data),action,
+    "Cencel",GTK_RESPONSE_CANCEL,"OK",GTK_RESPONSE_OK,NULL);
+    gint res;
+    res=gtk_dialog_run(GTK_DIALOG(dialog));
+    if(res==GTK_RESPONSE_OK){
+        char *filename;
+        filename=gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+        g_print("%s\n",filename);
+    }
+    gtk_widget_destroy(dialog);
+}
+
 static void gtkmain(GtkApplication *app,gpointer user_data){
+    //Main window
     GtkBuilder *builder=gtk_builder_new_from_file("window.ui");
     GObject *window=gtk_builder_get_object(builder,"window");
     gtk_application_add_window(app,GTK_WINDOW(window));
+    //dialog button
+    GObject *btn_dialog=gtk_builder_get_object(builder,"btnchoose");
+    g_signal_connect(btn_dialog,"clicked",G_CALLBACK(file_dialog),window);
     gtk_widget_show_all(GTK_WIDGET(window));
 }
 
