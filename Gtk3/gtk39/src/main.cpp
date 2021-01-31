44ee98df8c360bbf88023d3675e61449b797519e
daleclack <daleclack1@foxmail.com>
2021-01-31 14:05:41 +0800

    Add gtk39

---
+++ Gtk3/gtk39/src/main.cpp
@@ -0,0 +1,13 @@
+#include "gtk/gtk.h"
+
+static void gtkmain(GtkApplication *app,gpointer user_data){
+    GtkWidget *window=gtk_application_window_new(app);
+    gtk_widget_show_all(window);
+}
+
+int main(int argc,char *argv[]){
+    GtkApplication *app;
+    app=gtk_application_new("com.github.daleclack.gtk39",G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app,"activate",G_CALLBACK(gtkmain),NULL);
+    return g_application_run(G_APPLICATION(app),argc,argv);
+}
