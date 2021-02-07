372c15d6495e93068df48f872e44362e859fafce
daleclack <daleclack1@foxmail.com>
2021-02-07 11:31:02 +0800

    Add gtk4-icon

---
+++ icon-install/install-icon.sh
@@ -0,0 +1,3 @@
+sudo cp ./gtk4-icon.png /usr/share/icons/hicolor/48x48/apps/gtk4-icon.png
+sudo cp ./gtk4-icon.svg /usr/share/icons/hicolor/scalabale/apps/gtk4-icon.svg
+sudo cp ./gtk4-icon.svg /usr/share/icons/hicolor/symbolic/apps/gtk4-icon-symbolic.svg
