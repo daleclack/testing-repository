5ba5807e079a05d0bb54be83368fe361a86f3be3
daleclack <daleclack1@foxmail.com>
2021-02-09 18:31:58 +0800

    Update icon-install script

--- icon-install/install-icon.sh
+++ icon-install/install-icon.sh
@@ -1,5 +1,5 @@
 sudo cp ./gtk4-icon.png /usr/share/icons/hicolor/48x48/apps/gtk4-icon.png
 sudo cp ./gtk4-icon.svg /usr/share/icons/hicolor/scalabale/apps/gtk4-icon.svg
 sudo cp ./gtk4-icon.svg /usr/share/icons/hicolor/symbolic/apps/gtk4-icon-symbolic.svg
-sudo gtk-update-icon-cache
-sudo gtk4-update-icon-cache
+sudo gtk-update-icon-cache -f -t /usr/share/icons/hicolor
+sudo gtk4-update-icon-cache -f -t /usr/share/icons/hicolor
