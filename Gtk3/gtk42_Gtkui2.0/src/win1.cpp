faed8581485ccff077f01025ad83350e60ad3259
daleclack <daleclack1@foxmail.com>
2021-02-24 11:06:54 +0800

    Update GtkUi2.0(gtk42)

--- Gtk3/gtk42_Gtkui2.0/src/win1.cpp
+++ Gtk3/gtk42_Gtkui2.0/src/win1.cpp
@@ -4,5 +4,6 @@ void win1_init(GtkWidget *widget,GtkWindow *parent){
     GtkBuilder *win1=gtk_builder_new_from_resource("/gtk42/win1.ui");
     GObject *window1=gtk_builder_get_object(win1,"window1");
     gtk_window_set_transient_for(GTK_WINDOW(window1),parent);
+    g_signal_connect_swapped(window1,"response",G_CALLBACK(gtk_widget_destroy),window1);
     gtk_widget_show_all(GTK_WIDGET(window1));
 }
