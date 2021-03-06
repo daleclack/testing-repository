03e6cbb78dd654b8a2ed4578a989cd94a7f7da28
daleclack <daleclack1@foxmail.com>
2021-03-06 12:22:27 +0800

    Update gtk49

--- Gtk3/gtk49_entry/src/main.cpp
+++ Gtk3/gtk49_entry/src/main.cpp
@@ -1,17 +1,17 @@
 #include <gtk/gtk.h>
 
-void btnvisible_clicked(GtkWidget *widget,gpointer data){
+static void btnvisible_clicked(GtkWidget *widget,gpointer data){
     gtk_entry_set_visibility(GTK_ENTRY(data),gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget)));
 }
 
-void btnedit_clicked(GtkWidget *widget,gpointer data){
+static void btnedit_clicked(GtkWidget *widget,gpointer data){
     gtk_editable_set_editable(GTK_EDITABLE(data),gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget)));
 }
 
 static void gtkmain(GtkApplication *app,gpointer user_data){
     //Get window
     GtkBuilder *builder=gtk_builder_new_from_file("res/window.ui");
-    GObject *window,*btn_edit,*btn_visible,*entry;
+    GObject *window,*btn_edit,*btn_visible,*btn_exit,*entry;
     window=gtk_builder_get_object(builder,"window");
     //Get entry and buttons
     entry=gtk_builder_get_object(builder,"entry");
@@ -19,6 +19,8 @@ static void gtkmain(GtkApplication *app,gpointer user_data){
     g_signal_connect(btn_edit,"clicked",G_CALLBACK(btnedit_clicked),entry);
     btn_visible=gtk_builder_get_object(builder,"btnvisible");
     g_signal_connect(btn_visible,"clicked",G_CALLBACK(btnvisible_clicked),entry);
+    btn_exit=gtk_builder_get_object(builder,"btnExit");
+    g_signal_connect_swapped(btn_exit,"clicked",G_CALLBACK(gtk_widget_destroy),window);
     gtk_application_add_window(app,GTK_WINDOW(window));
     gtk_widget_show_all(GTK_WIDGET(window));
 }
