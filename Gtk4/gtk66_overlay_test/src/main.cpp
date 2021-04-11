c8d768ccbac1e734c6546edb30b766815ad14795
daleclack <daleclack1@foxmail.com>
2021-04-11 17:23:07 +0800

    Update gtk66

--- Gtk4/gtk66_overlay_test/src/main.cpp
+++ Gtk4/gtk66_overlay_test/src/main.cpp
@@ -1,19 +1,25 @@
-#include<gtk/gtk.h>
+#include <gtk/gtk.h>
+#include "src/img7.xpm"
 
 static void button_clicked(GtkWidget *widget,GtkApplication *app){
     //A text function of gtk_overlay_get_child
     GtkWindow *win=gtk_application_get_active_window(app);
-    GtkWidget *overlay,*label;
+    GtkWidget *overlay,*back;
     overlay=gtk_window_get_child(win);
-    label=gtk_overlay_get_child(GTK_OVERLAY(overlay));
-    gtk_label_set_label(GTK_LABEL(label),"Test");
+    back=gtk_overlay_get_child(GTK_OVERLAY(overlay));
+    GdkPixbuf *pixbuf=gdk_pixbuf_new_from_xpm_data(img7);
+    GdkPixbuf *sized=gdk_pixbuf_scale_simple(pixbuf,640,360,GDK_INTERP_BILINEAR);
+    gtk_picture_set_pixbuf(GTK_PICTURE(back),sized);
+    g_object_unref(pixbuf);
+    g_object_unref(sized);
+    //gtk_label_set_label(GTK_LABEL(label),"Test");
 }
 
 static void gtkmain(GtkApplication *app,gpointer user_data){
-    GtkWidget *window,*label1,*label2,*overlay,*button;
+    GtkWidget *window,*label1,*overlay,*button,*background;
     //Main window
     window=gtk_application_window_new(app);
-    gtk_window_set_default_size(GTK_WINDOW(window),400,100);
+    gtk_window_set_default_size(GTK_WINDOW(window),640,360);
     gtk_window_set_icon_name(GTK_WINDOW(window),"gtk66");
     //Overlay widgets
     overlay=gtk_overlay_new();
@@ -29,8 +35,9 @@ static void gtkmain(GtkApplication *app,gpointer user_data){
     gtk_widget_set_halign(label1,GTK_ALIGN_CENTER);
     gtk_overlay_add_overlay(GTK_OVERLAY(overlay),label1);
     //label2
-    label2=gtk_label_new("label2");
-    gtk_overlay_set_child(GTK_OVERLAY(overlay),label2);
+    //background=gtk_label_new("label2");
+    background=gtk_picture_new();
+    gtk_overlay_set_child(GTK_OVERLAY(overlay),background);
     gtk_window_set_child(GTK_WINDOW(window),overlay);
     gtk_widget_show(window);
 }
