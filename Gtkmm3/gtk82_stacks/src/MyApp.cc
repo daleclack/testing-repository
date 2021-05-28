77448bd07da814bfb6be9d9cd4fb56627f460ee5
daleclack <daleclack1@foxmail.com>
2021-05-28 16:17:08 +0800

    Update gtk82

--- Gtkmm3/gtk82_stacks/src/MyApp.cc
+++ Gtkmm3/gtk82_stacks/src/MyApp.cc
@@ -22,21 +22,36 @@ void MyApp::on_activate(){
     ref_builder->get_widget("main_window",main_window);
     ref_builder->get_widget("btn_1",btn1);
     ref_builder->get_widget("btn_2",btn2);
+    ref_builder->get_widget("btn_3",btn3);
+    ref_builder->get_widget("main_stack",main_stack);
+    ref_builder->get_widget("btn_pg2",btnpg2);
     ref_builder->get_widget("frame1",frame1);
     ref_builder->get_widget("frame2",frame2);
+    ref_builder->get_widget("frame3",frame3);
+    //Default Page
+    main_stack->set_visible_child(*frame1);
     //Click signal
     btn1->signal_clicked().connect(sigc::mem_fun(*this,&MyApp::btn1_clicked));
     btn2->signal_clicked().connect(sigc::mem_fun(*this,&MyApp::btn2_clicked));
+    btn3->signal_clicked().connect(sigc::mem_fun(*this,&MyApp::btn3_clicked));
+    btnpg2->signal_clicked().connect(sigc::mem_fun(*this,&MyApp::btnpg2_clicked));
     add_window(*main_window);
     main_window->show_all();
 }
 
 void MyApp::btn1_clicked(){
-    frame1->hide();
-    frame2->show();
+    
+    main_stack->set_visible_child(*frame2);
 }
 
 void MyApp::btn2_clicked(){
-    frame2->hide();
-    frame1->show();
+    main_stack->set_visible_child(*frame1);
+}
+
+void MyApp::btn3_clicked(){
+    main_stack->set_visible_child(*frame1);
+}
+
+void MyApp::btnpg2_clicked(){
+    main_stack->set_visible_child(*frame3);
 }
--- Gtkmm3/gtk82_stacks/src/MyApp.hh
+++ Gtkmm3/gtk82_stacks/src/MyApp.hh
@@ -13,9 +13,12 @@ private:
     //Main Builder
     Glib::RefPtr<Gtk::Builder> ref_builder;
     //Child widgets
-    Gtk::Button *btn1,*btn2;
-    Gtk::AspectFrame *frame1,*frame2;
+    Gtk::Button *btn1,*btn2,*btn3,*btnpg2;
+    Gtk::AspectFrame *frame1,*frame2,*frame3;
+    Gtk::Stack *main_stack;
     //Signal Handlers
     void btn1_clicked();
     void btn2_clicked();
+    void btn3_clicked();
+    void btnpg2_clicked();
 };
