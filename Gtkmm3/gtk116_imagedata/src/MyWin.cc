d6fe1b191e5869d2286c1441c985a0b48227a3fd
daleclack <daleclack1@foxmail.com>
2021-11-12 19:45:39 +0800

    Update gtk116

--- Gtkmm3/gtk116_imagedata/src/MyWin.cc
+++ Gtkmm3/gtk116_imagedata/src/MyWin.cc
@@ -1,35 +1,58 @@
 #include "MyWin.hh"
-#include <iostream>
+#include <cstdio>
 #include "image_types.hh"
 #include "winpe.xpm"
 
 MyWin::MyWin()
-:btn_back("Background")
+:btn_back("Open Image"),
+hbox(Gtk::ORIENTATION_HORIZONTAL,5),
+btnbox(Gtk::ORIENTATION_VERTICAL,5)
 {
     set_icon_name("org.gtk.daleclack");
-    background.set_size_request(640,360);
+    background.set_size_request(640,480);
     
     //Create a pixbuf
     pixbuf = Gdk::Pixbuf::create_from_xpm_data(winpe);
-    sized = pixbuf->scale_simple(640,360,Gdk::INTERP_BILINEAR);
+    sized = pixbuf->scale_simple(640,480,Gdk::INTERP_BILINEAR);
 
     //Add Button
     btn_back.set_halign(Gtk::ALIGN_CENTER);
     btn_back.set_valign(Gtk::ALIGN_CENTER);
-    m_overlay.add_overlay(btn_back);
+    btn_back.set_relief(Gtk::RELIEF_NONE);
     btn_back.signal_clicked().connect(sigc::mem_fun(*this,&MyWin::btnback_clicked));
+
+    //Add Gesture
+    gesture = Gtk::GestureMultiPress::create(draw_area);
+    gesture->set_button(1);
+    gesture->signal_pressed().connect(sigc::mem_fun(*this,&MyWin::gesture_pressed));
+    draw_area.set_hexpand();
+    draw_area.set_vexpand();
+    m_overlay.add_overlay(draw_area);
 
     //Set image
     gtk_image_set_from_pixbuf(background.gobj(),sized->gobj());
-    get_pixel_color(320,180);
+    //get_pixel_color(320,180);
+
+    //Add Color and Image Button
+    color_btn.set_use_alpha();
+    btnbox.pack_start(label_pos,Gtk::PACK_SHRINK);
+    btnbox.pack_start(label_rgba,Gtk::PACK_SHRINK);
+    btnbox.pack_start(color_btn,Gtk::PACK_SHRINK);
+    btnbox.pack_start(btn_back,Gtk::PACK_SHRINK);
+    btnbox.set_valign(Gtk::ALIGN_CENTER);
+
+    //Add Widgets and show all
     m_overlay.add(background);
-    add(m_overlay);
+    hbox.pack_start(m_overlay);
+    hbox.pack_start(btnbox,Gtk::PACK_SHRINK);
+    add(hbox);
     show_all_children();
     pixbuf.reset();
 }
 
 void MyWin::get_pixel_color(int x,int y){
     //Get Image data
+    Gdk::RGBA color_set;
     int red=0,green=0,blue=0,alpha=0;
     int n_channels = sized->get_n_channels();
     int rowstride = sized->get_rowstride();         //Rows
@@ -51,14 +74,25 @@ void MyWin::get_pixel_color(int x,int y){
             }
         }       
     }
-    red/=25;blue/=25;green/=25;alpha/=25;
+
+    //Set Color to rgba
+    color_set.set_red(red/25.0/255.0);
+    color_set.set_blue(blue/25.0/255.0);
+    color_set.set_green(green/25.0/255.0);
+    color_set.set_alpha(alpha/25.0/255.0);
+
+    //Get Color set as a string
+    label_rgba.set_label(color_set.to_string());
 
     //OutPut the color rgba set
-    if(pixbuf->get_has_alpha()){
-        std::cout<<red<<" "<<green<<" "<<blue<<" "<<alpha<<std::endl;
-    }else{
-        std::cout<<red<<" "<<green<<" "<<blue<<" "<<alpha<<std::endl;
-    }
+    color_btn.set_rgba(color_set);
+}
+
+void MyWin::gesture_pressed(int n_press,double x,double y){
+    char pos[57];
+    sprintf(pos,"(%.2f,%.2f)",x,y);
+    label_pos.set_label(pos);
+    get_pixel_color((int)x, (int)y);
 }
 
 void MyWin::btnback_clicked(){
@@ -98,7 +132,7 @@ void MyWin::set_background(int response_id){
         pixbuf = Gdk::Pixbuf::create_from_file(filename);
         //Release memory of pixbuf "sized"
         sized.reset();
-        sized = pixbuf->scale_simple(640,360,Gdk::INTERP_BILINEAR);
+        sized = pixbuf->scale_simple(640,480,Gdk::INTERP_BILINEAR);
         gtk_image_set_from_pixbuf(background.gobj(),sized->gobj());
         get_pixel_color(320,180);
     }
--- Gtkmm3/gtk116_imagedata/src/MyWin.hh
+++ Gtkmm3/gtk116_imagedata/src/MyWin.hh
@@ -10,13 +10,20 @@ class MyWin : public Gtk::Window{
         Gtk::Overlay m_overlay;
         Gtk::Button btn_back;
         Gtk::Image background;
+        Gtk::Box hbox,btnbox;
+        Gtk::Label label_pos,label_rgba;
+        Gtk::ColorButton color_btn;
+        Gtk::DrawingArea draw_area;
         Glib::RefPtr<Gdk::Pixbuf> pixbuf;
         Glib::RefPtr<Gdk::Pixbuf> sized;
+
         //Get Mouse Position
-        Glib::RefPtr<Gtk::Gesture> gesture;
+        Glib::RefPtr<Gtk::GestureMultiPress> gesture;
+        
         //Signal Handlers
         Glib::RefPtr<Gtk::FileChooserNative> dialog;
         void get_pixel_color(int x,int y);
         void btnback_clicked();
         void set_background(int reponse_id);
+        void gesture_pressed(int n_press,double x,double y);
 };
