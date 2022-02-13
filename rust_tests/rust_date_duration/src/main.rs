479973977e301d6cb4f90d80b8769e7f645be7a4
daleclack <daleclack1@foxmail.com>
2022-02-13 17:40:30 +0800

    Add comments for rust_duration

--- rust_tests/rust_date_duration/src/main.rs
+++ rust_tests/rust_date_duration/src/main.rs
@@ -1,10 +1,14 @@
 use chrono::*;
 
 fn main() {
+    // Get current time
     let now = Utc::now();
     let naive_time = NaiveDate::from_ymd(2017,5,19).and_hms(0, 0, 0);
     let other_dt = DateTime::<Utc>::from_utc(naive_time,Utc);
     
+    // Calculate the duration time
     let diff = now.signed_duration_since(other_dt);
+
+    // Just print it on terminal
     println!("{}",diff.num_days());
 }
