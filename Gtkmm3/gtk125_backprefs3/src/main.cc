ddfb779ea2d7d02af88aa9fdef9c2189e3b1bfcf
daleclack <daleclack1@foxmail.com>
2022-02-27 10:55:59 +0800

    Update gtk125

--- Gtkmm3/gtk125_backprefs3/src/main.cc
+++ Gtkmm3/gtk125_backprefs3/src/main.cc
@@ -1,10 +1,12 @@
 #include "MyWin.hh"
+#define GETTEXT_PACKAGE "gtk125"
+#define PROGRAMNAME_LOCALEDIR "./po"
 
 int main(int argc,char ** argv){
     // Initalize gettext
-    // bindtextdomain(GETTEXT_PACKAGE, PROGRAMNAME_LOCALEDIR);
-    // bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
-    // textdomain(GETTEXT_PACKAGE);
+    bindtextdomain(GETTEXT_PACKAGE, PROGRAMNAME_LOCALEDIR);
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain(GETTEXT_PACKAGE);
 
     // Create application and run
     auto app = Gtk::Application::create(argc,argv,"org.gtk.daleclack");
