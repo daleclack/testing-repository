af6ae70796b82ae3dbbddae26948ea1d9f9b9597
daleclack <daleclack1@foxmail.com>
2022-03-10 13:27:19 +0800

    Add gtk125

--- Gtkmm3/gtk125_backprefs3/src/main.cc
+++ Gtkmm3/gtk125_backprefs3/src/main.cc
@@ -4,6 +4,7 @@
 
 int main(int argc,char ** argv){
     // Initalize gettext
+    //setlocale(LC_ALL,"");
     bindtextdomain(GETTEXT_PACKAGE, PROGRAMNAME_LOCALEDIR);
     bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
     textdomain(GETTEXT_PACKAGE);
