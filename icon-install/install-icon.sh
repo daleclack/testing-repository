cc9adb5c249aa011d6762db695e5a2ea563bb558
daleclack <daleclack1@foxmail.com>
2022-09-03 13:57:34 +0800

    Update icon install script

--- icon-install/install-icon.sh
+++ icon-install/install-icon.sh
@@ -1,6 +1,8 @@
-sudo cp ./org.gtk.daleclack.png /usr/share/icons/hicolor/48x48/apps/org.gtk.daleclack.png
-sudo cp ./org.gtk.daleclack.svg /usr/share/icons/hicolor/scalable/apps/org.gtk.daleclack.svg
-sudo cp ./org.gtk.daleclack.svg /usr/share/icons/hicolor/symbolic/apps/org.gtk.daleclack.svg
+mkdir ~/.local/share/icons/hicolor/scalable/apps
+mkdir ~/.local/share/icons/hicolor/symbolic/apps
+cp ./org.gtk.daleclack.png ~/.local/share/icons/hicolor/48x48/apps/org.gtk.daleclack.png
+cp ./org.gtk.daleclack.svg ~/.local/share/icons/hicolor/scalable/apps/org.gtk.daleclack.svg
+cp ./org.gtk.daleclack.svg ~/.local/share/icons/hicolor/symbolic/apps/org.gtk.daleclack.svg
 cp ./org.gtk.daleclack.desktop ~/.local/share/applications
-sudo gtk-update-icon-cache -f -t /usr/share/icons/hicolor
-sudo gtk4-update-icon-cache -f -t /usr/share/icons/hicolor
+gtk-update-icon-cache -f -t ~/.local/share/icons/hicolor
+gtk4-update-icon-cache -f -t ~/.local/share/icons/hicolor
