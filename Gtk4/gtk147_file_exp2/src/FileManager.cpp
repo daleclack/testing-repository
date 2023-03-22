36fdc9b7e31fa3ddfd56ba7c4973bf982ae9149e
daleclack <daleclack1@foxmail.com>
2023-03-22 15:19:25 +0800

    Update gtk147

--- Gtk4/gtk147_file_exp2/src/FileManager.cpp
+++ Gtk4/gtk147_file_exp2/src/FileManager.cpp
@@ -7,7 +7,19 @@ struct _FileManager{
 G_DEFINE_TYPE(FileManager, file_manager, GTK_TYPE_WINDOW)
 
 static void file_manager_init(FileManager *self){
+    // Create file for directory
+    GFile *file = g_file_new_for_path(g_get_home_dir());
 
+    // Create List item factory
+    GtkListItemFactory *factory = gtk_signal_list_item_factory_new();
+
+    // Create list model
+    GtkDirectoryList *list_model = gtk_directory_list_new("local_only", file);
+    GtkWidget *view = gtk_list_view_new(GTK_SELECTION_MODEL(
+                      gtk_single_selection_new(G_LIST_MODEL(list_model))), factory);
+    
+    // Add widget to the window
+    gtk_window_set_child(GTK_WINDOW(self), view);
 }
 
 static void file_manager_class_init(FileManagerClass *klass){
