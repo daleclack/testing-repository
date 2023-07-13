c5aa0d06053cbb1111910fcb120c0e747acbc2dc
daleclack <daleclack1@foxmail.com>
2023-07-13 15:01:27 +0800

    Add qemu start scripts

---
+++ termux/win81PE_5.sh
@@ -0,0 +1,23 @@
+# Options:
+# -m Random Access Memory
+# -cpu for cpu type
+# -M q35 for sata bus
+# -accel for acceleration
+# -drive for disk/cdrom
+# -boot for boot device:a-floppy,c-disk,fd-cdrom
+# -vnc for vnc display
+# -usb -device usb-tablet for fix mouse
+cd /sdcard/0
+# start qemu
+qemu-system-x86_64 \
+-m 1024 \
+-cpu max \
+-M q35 \
+-accel tcg,thread=multi \
+-drive file=Pagefile.vhd,media=disk \
+-drive file=Cpp.vhd,media=disk \
+-drive file=Win8.1_PE_x64_5.iso,media=cdrom \
+-drive file=Dev-Cpp-UCRT64.iso,media=cdrom \
+-boot d \
+-vnc :1 \
+-usb -device usb-tablet
