99a7dc1d4344d0247b5a3a7823edb369a9355eee
daleclack <daleclack1@foxmail.com>
2023-08-02 20:34:26 +0800

    Add qemu scripts

---
+++ termux/termux-qemu/winxpe3-fs6.sh
@@ -0,0 +1,24 @@
+# Options:
+# -m Random Access Memory
+# -cpu for cpu type
+# -smp for core count
+# -M q35 for sata bus
+# -accel for acceleration
+# -drive for disk/cdrom
+# -boot for boot device:a-floppy,c-disk,fd-cdrom
+# -vnc for vnc display
+# -usb -device usb-tablet for fix mouse
+cd /sdcard/0
+# start qemu
+qemu-system-x86_64 \
+-m 672 \
+-M q35 \
+-accel tcg,thread=multi \
+-drive file=Pagefile.vhd,media=disk \
+-drive file=Cpp.vhd,media=disk \
+-drive file=winxpe3-fs6.iso,media=cdrom \
+-drive file=music_disc.iso,media=cdrom \
+-boot d \
+-vnc :1 \
+-device ac97 
+# -usb -device usb-tablet
