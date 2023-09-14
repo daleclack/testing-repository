24ef0ddd28a2faf02e8da904859f7a4e221423af
daleclack <daleclack1@foxmail.com>
2023-09-14 21:16:02 +0800

    Add commits

--- Gtk4/gtk149_file_exp3/src/GetFileType.h
+++ Gtk4/gtk149_file_exp3/src/GetFileType.h
@@ -1,13 +1,17 @@
+/*
+Patch for Microsoft Windows
+*/
 #pragma once
 
 #include <gtk/gtk.h>
 #include <cstring>
 
 // Check the whether the selected file is directory
-// This is a patch for Microsoft Windows
 static inline gboolean check_directory(GFileInfo *info)
 {
 #ifdef _WIN32
+    // Since all files for windows will be "unknown" type, 
+    // use content type to check file.
     const char *file_type = g_file_info_get_content_type(info);
     if(strncmp(file_type, "inode/directory", strlen("inode/directory")) == 0)
     {
