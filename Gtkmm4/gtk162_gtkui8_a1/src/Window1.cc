be6d4ea3c1c48ca9e08a3b2cea027464f00d8e7e
daleclack <daleclack1@foxmail.com>
2025-01-29 19:32:36 +0800

    Remove debug information

--- Gtkmm4/gtk162_gtkui8_a1/src/Window1.cc
+++ Gtkmm4/gtk162_gtkui8_a1/src/Window1.cc
@@ -7,7 +7,7 @@ Window1::Window1()
 
 bool Window1::on_close_request()
 {
-    g_print("The window is closing\n");
+    // g_print("The window is closing\n");
     set_visible(false);
     return true;
 }
